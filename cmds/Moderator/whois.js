f6cbc334b2ef767c56e46d519f1e78c9bbe195a6
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2018-01-27 15:25:54 +0200

    Fixed whois to work without a mention

--- cmds/Moderator/whois.js
+++ cmds/Moderator/whois.js
@@ -5,7 +5,7 @@ exports.data = {
 	command: 'whois',
 	syntax: 'whois [user mention]',
 	author: 'Aris A.',
-	permissions: 2,
+	permissions: 1,
 	anywhere: false
 };
 
@@ -13,42 +13,82 @@ exports.func = async (msg,args) => {
 	const log = require(`${msg.client.config.folders.lib}/log.js`)('Whois');
 	try{
 		if (args[0]){
-			var UserID = await msg.mentions.users.first().id;
-			var UserStatus = await msg.mentions.users.first().presence.status;
-			//var Game = await msg.mentions.users.first().presence.game.toString();
-			var Username = await msg.mentions.users.first().username;
-			var Avatar = await msg.mentions.users.first().avatarURL;
-			var Discriminator = await msg.mentions.users.first().discriminator;
-			msg.channel.send('', {embed: {
-				thumbnail: {url: Avatar},
-				author: {
-					name: `Information about ${Username}#${Discriminator}`,
-					icon_url: Avatar
-				},
-				color: 0xff8000 ,
-				fields: [{
-					name: 'User ID',
-					value: UserID
-				},
-				{
-					name: 'Username',
-					value: Username
-				},    
-				{
-					name: 'Status',
-					value: UserStatus
-				},               
-				{
-					name: 'Avatar Link',
-					value: Avatar
+			if (args[0].includes('@')){
+				if (msg.mentions.users.first().id != null){
+					//var Game = await msg.mentions.users.first().presence.game.toString();
+					msg.channel.send('', {embed: {
+						thumbnail: {url: msg.mentions.users.first().avatarURL},
+						author: {
+							name: `Information about ${msg.mentions.users.first().tag}`,
+							icon_url: msg.mentions.users.first().avatarURL
+						},
+						color: 0xff8000 ,
+						fields: [{
+							name: 'User ID',
+							value: msg.mentions.users.first().id
+						},
+						{
+							name: 'Username',
+							value: msg.mentions.users.first().username
+						},    
+						{
+							name: 'Status',
+							value: msg.mentions.users.first().presence.status
+						},               
+						{
+							name: 'Avatar Link',
+							value: msg.mentions.users.first().avatarURL
+						}
+						],
+					}});
+				} else {
+					msg.channel.send('User does not exist.');
 				}
-				],
-			}});
+			} else {
+				const Users = await msg.guild.members;
+				var Found = false;
+				let User;
+				await Users.forEach(user => {
+					if ( (user.user.username.toLowerCase() == args[0].toLowerCase()) || ( user.nickname && (user.nickname.toLowerCase() == args[0].toLowerCase()))) {
+						Found = true;
+						User = user;
+					}
+				});
+				if (Found == true) {
+					msg.channel.send('', {embed: {
+						thumbnail: {url: User.user.avatarURL},
+						author: {
+							name: `Information about ${User.user.tag}`,
+							icon_url: User.user.avatarURL
+						},
+						color: 0xff8000 ,
+						fields: [{
+							name: 'User ID',
+							value: User.user.id
+						},
+						{
+							name: 'Username',
+							value: User.user.username
+						},    
+						{
+							name: 'Status',
+							value: User.user.presence.status
+						},               
+						{
+							name: 'Avatar Link',
+							value: User.user.avatarURL
+						}
+						],
+					}});
+				} else {
+					msg.channel.send('User does not exist.');
+				}
+			}
 		} else {
 			msg.reply('You did not provide a @User.');
 		}
 	} catch (err) {
 		msg.reply('Something went wrong.');
-		log.error(`Sorry ${msg.author}, could not find user info: ${err}`);
+		log.error(`Sorry ${msg.author.tag}, could not find user info: ${err}`);
 	}
 };
