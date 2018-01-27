3ff0feec2cc1b80bce1161406f40b54921eb7163
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2018-01-27 16:18:42 +0200

    Fic for update.js

--- cmds/System/update.js
+++ cmds/System/update.js
@@ -14,7 +14,7 @@ exports.func = async (msg) => {
 	try {
 		const sent = await msg.channel.send('Updating code...');
 		const response = await exec(`git pull https://${msg.client.auth.Github.Username}:${msg.client.auth.Github.Password}@github.com/pamehabai6/Tron.git`);
-		if(response.stdout.toString().includes('Already up to date.')){
+		if(response.stdout.toString().includes('Already up-to-date.') || response.stdout.toString().includes('Already up to date.')){
 			await sent.edit('The code is already up to date!');
 			return;
 		} else {
