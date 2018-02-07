340288767a230d4cd80ee0d315813a123be61ebe
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2018-02-07 18:52:01 +0200

    Made on message synchronous

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ bot.config = require('./config.json');
 bot.auth = require('./auth.json');
 
 // Basic Function Modules
-const fs = require('fs');
 const log = require(`${bot.config.folders.lib}/log.js`)('Core');
 bot.Watcher = require(`${bot.config.folders.models}/watcher.js`);
 const chalk = require('chalk');
@@ -19,7 +18,6 @@ bot.CMDModel = require(`${bot.config.folders.models}/commands.js`);
 const Profiles = require(`${bot.config.folders.models}/profiles.js`);
 const OTS = require(`${bot.config.folders.models}/otsroles.js`);
 
-const wordlist = bot.config.folders.wordlist;
 // ==== Initialisation ====
 bot.db = Database.start(); // Start the database and connect
 
@@ -71,87 +69,60 @@ bot.on('ready', async () => {
 });
 
 // When message is received
-bot.on('message', async msg => {
+bot.on('message', msg => {
 	try {
 		var timer = process.hrtime();
 		// Reject message if the message author is a bot or the message is not in a guild (eg. DMs)
 		if (msg.author.bot || !msg.guild) return;
 		// Find message's guild in the database
-		msg.server = await bot.Server.findOne({ 
-			where: {
-				guildId: msg.guild.id
-			}
-		});
-		let command;
-		let args;
-		// Loop through possible prefixes to check if message is a command - this is a bit confusing because if the message is a command, then it is set to false (this is just so I could use .every())
-		const notCommand = [msg.server.altPrefix, bot.config.prefix, `<@${bot.user.id}>`, `<@!${bot.user.id}>`].every(prefix => {
-			if (msg.content.toLowerCase().startsWith(prefix)) { // Check if message starts with prefix
-				command = msg.content.slice(prefix.length).trim().split(' ')[0]; // Get the name of the command
-				args = msg.content.slice(prefix.length).trim().split(' ').slice(1); // Get the args of the command
-				return false;
-			}
-			return true;
-		});
-
-		if (notCommand) {
-			//Check if user exists in db
-			const userExists = await Profiles.findOne({
-				where: {
-					guildId: msg.guild.id,
-					username: msg.author.username
+		getServer(bot,msg).then( (msgserver) => {
+			let command, args;
+			// Loop through possible prefixes to check if message is a command - this is a bit confusing because if the message is a command, then it is set to false (this is just so I could use .every())
+			const notCommand = [msgserver.altPrefix, bot.config.prefix, `<@${bot.user.id}>`, `<@!${bot.user.id}>`].every(prefix => {
+				if (msg.content.toLowerCase().startsWith(prefix)) { // Check if message starts with prefix
+					command = msg.content.slice(prefix.length).trim().split(' ')[0]; // Get the name of the command
+					args = msg.content.slice(prefix.length).trim().split(' ').slice(1); // Get the args of the command
+					return false;
 				}
+				return true;
 			});
-			//If user exists
-			if (userExists){
-				//Get message count and add one more
-				var UserCount = userExists.msgcount + 1;
-				await userExists.update({
-					msgcount: UserCount
+	
+			if (notCommand) {
+				updateUser(msg).then( () => {
+					return;
 				});
+				return;
+			}
+	
+			let cmd;
+			// Check whether command exists as a file. (loaded in the commands collection)
+			if (bot.commands.has(command)) { 
+				// Fetch the command's prototype
+				cmd = bot.commands.get(command); 
 			} else {
-				//Else create user entry in db
-				await Profiles.create({
-					guildId: msg.guild.id,
-					username: msg.author.username,
-					discordid: msg.author.id
+				msg.reply('Command does not exist.');
+			}
+			//Check if command is registered in the database.
+			getCMDModel(bot,msg, command).then((cmdExists) => {
+				//If it is disabled return.
+				if (cmdExists && cmdExists.enabled == false){
+					msg.reply(`Command is disabled in ${msg.guild.name}.`);
+					return;
+				}
+				// Sometimes a message doesn't have a member object attached (idk either like wtf)
+				if (!msg.member) { 
+					msg.member = msg.guild.fetchMember(msg);
+				}
+				// Get user's permission level
+				bot.elevation(msg).then( (msgelevation) => {
+					if (msgelevation >= cmd.data.permissions) { // Check that the user exceeds the command's required elevation
+						cmd.func(msg, args, bot); // Run the command's function
+					} else {
+						msg.reply(':scream: You don\'t have permission to use this command.');
+					}
 				});
-				log.info(`Created db entry for user ${msg.author.username}.`);
-			}
-			return;
-		}
-
-		let cmd;
-		// Check whether command exists as a file. (loaded in the commands collection)
-		if (bot.commands.has(command)) { 
-			// Fetch the command's prototype
-			cmd = bot.commands.get(command); 
-		} else {
-			msg.reply('Command does not exist.');
-		}
-		//Check if command is registered in the database.
-		const cmdExists = await bot.CMDModel.findOne({where: {
-			guildId: msg.guild.id,
-			name: command
-		}});
-		//If it is disabled return.
-		if (cmdExists && cmdExists.enabled == false){
-			msg.reply(`Command is disabled in ${msg.guild.name}.`);
-			return;
-		}
-		// Sometimes a message doesn't have a member object attached (idk either like wtf)
-		if (!msg.member) { 
-			msg.member = await msg.guild.fetchMember(msg);
-		}
-		// Get user's permission level
-		msg.elevation = await bot.elevation(msg);
-		if (cmd) { // Command is flagged to be used anywhere/user has 3+ elevation level/is in a permitted channel
-			if (msg.elevation >= cmd.data.permissions) { // Check that the user exceeds the command's required elevation
-				cmd.func(msg, args, bot); // Run the command's function
-			} else {
-				msg.reply(':newspaper2: You don\'t have permission to use this command.');
-			}
-		}
+			});
+		});
 		log.info(`${process.hrtime(timer)[0]}s, ${(process.hrtime(timer)[1] / 1000000).toFixed(3)}ms`);
 	} catch (err) {
 		log.error(`Something went wrong when handling a message: ${err}`);
@@ -231,11 +202,48 @@ bot.createCommands = (guild, id) => {
 	});
 };
 
-//Random word function
-bot.getWord = () => {
-	var word = fs.readFileSync(wordlist, 'utf8').split('\n');
-	return word[parseInt(Math.random()*word.length+1)];
-};
+async function updateUser (msg){
+	//Check if user exists in db
+	const userExists = await Profiles.findOne({
+		where: {
+			guildId: msg.guild.id,
+			username: msg.author.username
+		}
+	});
+	//If user exists
+	if (userExists){
+		//Get message count and add one more
+		var UserCount = userExists.msgcount + 1;
+		await userExists.update({
+			msgcount: UserCount
+		});
+	} else {
+		//Else create user entry in db
+		await Profiles.create({
+			guildId: msg.guild.id,
+			username: msg.author.username,
+			discordid: msg.author.id
+		});
+		log.info(`Created db entry for user ${msg.author.username}.`);
+	}
+	return;
+}
+
+async function getServer(bot,msg){
+	return await bot.Server.findOne({ 
+		where: {
+			guildId: msg.guild.id
+		}
+	});
+}
+
+async function getCMDModel(bot,msg, command){
+	return await bot.CMDModel.findOne({where: {
+		guildId: msg.guild.id,
+		name: command
+	}});
+}
+
 
 //Function that stops bot
 bot.stop = () => {
