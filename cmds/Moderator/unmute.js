57c14b0734b22cb0bba4bfd093e924e98e91d9e0
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2018-06-27 14:07:34 +0300

    Minor unmute changes

--- cmds/Moderator/unmute.js
+++ cmds/Moderator/unmute.js
@@ -12,23 +12,34 @@ exports.func = async (msg, args) => {
 	const OTS = require(`${msg.client.config.folders.models}/mute.js`);
 	const log = require(`${msg.client.config.folders.lib}/log.js`)('Unmute');
 	try{
-		if (args[0]){    
-			if (msg.mentions.users.first() != null){
-				let member = msg.mentions.members.first();
-				const ID = await OTS.findOne({
-					where: {
-						guildId: msg.channel.guild.id
+		if (args[0]){
+			let Member;
+			if (args[0].includes('@')){
+				Member = msg.mentions.members.first();
+			} else {
+				const Users = await msg.guild.members;
+				var Found = false;
+				await Users.forEach(user => {
+					if ( (user.user.username.toLowerCase() == args[0].toLowerCase()) || ( user.nickname && (user.nickname.toLowerCase() == args[0].toLowerCase()))) {
+						Found = true;
+						Member = user;
 					}
 				});
-				if (ID) {
-					const botspam = msg.guild.channels.get(ID.botspamChannelId);
-					await member.removeRole(ID.roleId);
-					await botspam.send(`${member} You have been unmuted by ${msg.author.tag}.`);
-				} else {
-					msg.reply(`Muted Role has not been set in ${msg.guild.name}.`);
+				if (!Found) {
+					return msg.channel.send('User does not exist.');
 				}
+			}
+			const ID = await OTS.findOne({
+				where: {
+					guildId: msg.channel.guild.id
+				}
+			});
+			if (ID) {
+				const botspam = msg.guild.channels.get(ID.botspamChannelId);
+				await Member.removeRole(ID.roleId);
+				await botspam.send(`${Member} You have been unmuted by ${msg.author.tag}.`);
 			} else {
-				return msg.reply('Please mention a valid member of this server.');
+				msg.reply(`Muted Role has not been set in ${msg.guild.name}.`);
 			}
 		} else {
 			msg.reply('You did not provide a name.');
