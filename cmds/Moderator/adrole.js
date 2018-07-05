b5d5a8c9ac043f0921433694d4fb1bd55bf8e9ed
Sqbika <sqbika@gmail.com>
2018-07-05 12:06:16 +0200

    Cleaned up command.

--- cmds/Moderator/adrole.js
+++ cmds/Moderator/adrole.js
@@ -5,73 +5,32 @@ exports.data = {
 	command: 'adrole',
 	syntax: 'adrole [@name | name] [Role]',
 	author: 'Aris A.',
+	revisiedBy: 'Sqbika',
 	permissions: 3,
 };
 
 exports.func = async (msg, args) => {
 	const log = require(`${msg.client.config.folders.lib}/log.js`)('AddRole');
-	try{
-		if (args[0]){
-			if (args[1]){
-				let Member;
-				if (args[0].includes('@')){
-					if (msg.mentions.users.first() != null){
-						Member = msg.mentions.members.first();
-					} else {
-						return msg.reply('Please mention a valid member of this server.');
-					}
-				} else {
-					const Users = await msg.guild.members;
-					var Found = false;
-					await Users.forEach(user => {
-						if ( (user.user.username.toLowerCase() == args[0].toLowerCase()) || ( user.nickname && (user.nickname.toLowerCase() == args[0].toLowerCase()))) {
-							Found = true;
-							Member = user;
-						}
-					});
-					if (!Found) {
-						return msg.reply('User does not exist.');
-					}
-				}
-				var RoleName = args[1];
-				if (args.length > 2){
-					for (var i = 2; i < args.length; i++) {
-						RoleName = RoleName + ' ' + args[i];
-					}
-				}
-				let Role;
-				msg.guild.roles.forEach(element => {
-					if(element.name.toLowerCase() == RoleName.toLowerCase()){
-						Role = element;
-					}
-				});
-				if (Role) {
-					if (Role.comparePositionTo(msg.member.highestRole) <= 0){
-						if (!Member.roles.has(Role.id)) {
-							let Error = false;
-							await Member.addRole(Role).catch(err => {
-								log.error(err);
-								Error = true;
-								msg.reply(`Role \`\`${Role}\`\` is higher than the Bots Role. Not enough permissions!`);
-							});
-							if (Error == false){
-								msg.reply(`Added role \`\`${RoleName}\`\` to \`\`${Member.user.username}\`\``);
-							}
-						} else {
-							msg.reply(`User already has the \`\`${Role.name}\`\` role!`);
-						}
-					} else {
-						msg.reply(`Role \`\`${Role.name}\`\` is higher than your \`\`${msg.member.highestRole.name}\`\` role.`);
-					}
-				} else {
-					return msg.reply('Role does not exist!');
-				}
-			} else {
-				msg.reply('You did not provide a Role.');
-			}
-		} else {
-			msg.reply('You did not provide a name.');
+	try {
+		if (args[0] == undefined) return msg.reply('You did not provide a name.');
+		if (args[1] == undefined) return msg.reply('You did not provide a Role.');
+		let Member;
+		if (msg.mentions.users.first() != undefined) Member = msg.mentions.members.first();
+		else {
+			Member = msg.guild.members.find((user) => user.user.username.toLowerCase() == args[0].toLowerCase() || (user.nickname && (user.nickname.toLowerCase() == args[0].toLowerCase())));
+			if (Member == undefined) return msg.reply('User does not exist.');
 		}
+		var RoleName = args.slice(1).join(' ');
+		let Role = msg.guild.roles.find(element => element.name.toLowerCase() == RoleName.toLowerCase());
+		if (Role == undefined) return msg.reply('Role does not exist!');
+		if (Role.comparePositionTo(msg.member.highestRole) > 0) return msg.reply(`Role \`\`${Role.name}\`\` is higher than your \`\`${msg.member.highestRole.name}\`\` role.`);
+		if (Member.roles.has(Role.id)) return msg.reply(`User already has the \`\`${Role.name}\`\` role!`);
+		await Member.addRole(Role).then(res => {
+			return msg.reply(`Added role \`\`${RoleName}\`\` to \`\`${Member.user.username}\`\``);
+		}).catch(err => {
+			log.error(err);
+			return msg.reply(`Role \`\`${Role}\`\` is higher than the Bots Role. Not enough permissions!`);
+		});
 	} catch (err) {
 		msg.reply('Something went wrong.');
 		log.error(`Sorry ${msg.author.tag} something went wrong: ${err}`);
