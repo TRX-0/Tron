1bf2bd4926374b161e59aff6173d0ddb38557cc7
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2019-08-02 20:20:18 +0300

    Added error logging

--- cmds/Utilities/eval.js
+++ cmds/Utilities/eval.js
@@ -34,7 +34,7 @@ exports.func = async (msg, args) => {
 				//	return msg.reply('Sorry, but I didn\'t receive a key from Hastebin.');
 				//}
 				return msg.reply(`Sorry, but your request was so big that I had to upload it: ${r.body.text()}`);
-			}).catch(() => msg.reply('Sorry, but an error happened with Pastebin!'));
+			}).catch(err => msg.reply(`Sorry, but an error happened with Pastebin: ${err}`));
 		} else {
 			await msg.channel.send('```xl\n' + clean(evaled) + '\n```',{split: {maxLength: 1950, char: 's'}}).catch(err => log.error(err));
 		}
