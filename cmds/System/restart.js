6d438fcaa583b28f105e71916ad77c9dcfe5438d
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2019-10-08 14:33:22 +0300

    Stop and restart are now async

--- cmds/System/restart.js
+++ cmds/System/restart.js
@@ -8,12 +8,12 @@ exports.data = {
 	permissions: 4
 };
 
-exports.func = (msg, args, bot) => {
+exports.func = async (msg, args, bot) => {
 	const log = require(`${bot.config.folders.lib}/log.js`)('Restart');
 	const exec = require('util').promisify(require('child_process').exec);
 	try {
-		msg.channel.send('Restarting all processes!');
-		exec(`cd ${bot.config.folders.home} && node main.js`);
+		await msg.channel.send('Restarting all processes!');
+		await exec(`cd ${bot.config.folders.home} && node main.js`);
 		bot.destroy();
 		process.exit();
 	} catch (err) {
--- cmds/System/stop.js
+++ cmds/System/stop.js
@@ -8,10 +8,10 @@ exports.data = {
 	permissions: 3
 };
 
-exports.func = (msg, args, bot) => {
+exports.func = async (msg, args, bot) => {
 	const log = require(`${bot.config.folders.lib}/log.js`)('Stop');
 	try {
-		msg.channel.send('Stopping all processes and exiting!');
+		await msg.channel.send('Stopping all processes and exiting!');
 		bot.destroy();
 		process.exit();
 	} catch (err) {
