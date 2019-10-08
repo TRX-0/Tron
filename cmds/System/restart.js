22216b658ded45fc4ca3f802d14ac34ca53986e3
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2019-10-08 14:31:22 +0300

    Refactored stop and restard

--- cmds/System/restart.js
+++ cmds/System/restart.js
@@ -12,19 +12,12 @@ exports.func = (msg, args, bot) => {
 	const log = require(`${bot.config.folders.lib}/log.js`)('Restart');
 	const exec = require('util').promisify(require('child_process').exec);
 	try {
-		return new Promise((resolve, reject) => {
-			try {
-				msg.channel.send('Restarting all processes!');
-				exec(`cd ${bot.config.folders.home} && node main.js`);
-				bot.destroy();
-				process.exit();
-			} catch (err) {
-				log.error(`Error on bot quit: ${err}`);
-				reject(err);
-			}
-		});
+		msg.channel.send('Restarting all processes!');
+		exec(`cd ${bot.config.folders.home} && node main.js`);
+		bot.destroy();
+		process.exit();
 	} catch (err) {
 		msg.reply('Something went wrong.');
-		log.error(`Something went wrong in stop.js: ${err}`);
+		log.error(`Error on bot restart: ${err}`);
 	}
 };
--- cmds/System/stop.js
+++ cmds/System/stop.js
@@ -11,18 +11,11 @@ exports.data = {
 exports.func = (msg, args, bot) => {
 	const log = require(`${bot.config.folders.lib}/log.js`)('Stop');
 	try {
-		return new Promise((resolve, reject) => {
-			try {
-				msg.channel.send('Stopping all processes and exiting!');
-				bot.destroy();
-				process.exit();
-			} catch (err) {
-				log.error(`Error on bot quit: ${err}`);
-				reject(err);
-			}
-		});
+		msg.channel.send('Stopping all processes and exiting!');
+		bot.destroy();
+		process.exit();
 	} catch (err) {
 		msg.reply('Something went wrong.');
-		log.error(`Something went wrong in stop.js: ${err}`);
+		log.error(`Error on bot quit: ${err}`);
 	}
 };
