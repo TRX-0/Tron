030565bec49b193867c36cf0a6a00e4de8ccafd0
Aristidis Angelopoulos <aristidis.angelopoulos@outlook.com>
2019-10-24 12:46:47 +0300

    Test
    Test
    
    
    Test
    
    
    Test
    
    
    tEST
    
    
    test
    
    
    test
    
    
    test
    
    
    test
    
    
    τεστ
    
    
    test
    
    
    Pretty good outcome
    
    
    test
    
    
    test
    
    
    test
    
    
    test
    
    
    test
    
    
    test
    
    
    test

--- cmds/Utilities/lottery.js
+++ cmds/Utilities/lottery.js
@@ -3,23 +3,38 @@ exports.data = {
 	command: 'lottery',
 	description: 'Pick random re-tweets',
 	group: 'Utilities',
-	syntax: 'lottery [tweet id] [number]',
+	syntax: 'lottery [tweet id] [amount]',
 	author: 'Aris A.',
 	permissions: 3,
 };
 
 exports.func = async (msg, args, bot) => {
-	const log = require(`${bot.config.folders.lib}/log.js`)('Watcher');
+	const log = require(`${bot.config.folders.lib}/log.js`)('Lottery');
 	const auth = require ('../../auth.json');
 	const Twit = require('twit');
 	const T = new Twit(auth.twitter);
-	const id = args[1];
-	const number = args[2];
-	try {
-		var Tweets = await T.get('statuses/retweeters/ids',{id: id,count:100,cursor:-1});
-		msg.channel.send(Tweets);
-	} catch (err) {
-		msg.reply('Something went wrong.');
-		log.error(`Error in the watcher function: ${err}`);
+	if(args.length == 2){
+		if (/^\d+$/.test(args[0]) && /^\d+$/.test(args[1])) {
+			const id = args[0];
+			const amount = args[1];
+			try {
+				let next = -1;
+				do {
+					T.get('statuses/retweeters/ids', { id: id, count: 100, cursor: next }, function(err, data, response) {
+						console.log(next);
+						console.log("==============");
+						console.log(data);
+						next = data.next_cursor;
+					});
+				} while (next > 0);
+			} catch (err) {
+				msg.reply('Something went wrong.');
+				log.error(`Error in the lottery function: ${err}`);
+			}
+		} else {
+			msg.reply("Twit id and amount must be numerals.");
+		}
+	} else {
+		msg.reply("Not enough arguments.");
 	}
 };
