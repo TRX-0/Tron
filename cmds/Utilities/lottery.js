c5aa54d2d83d0a3024734ef5a97eaec2d2caedad
Aristidis Angelopoulos <aristidis.angelopoulos@outlook.com>
2019-10-24 11:51:02 +0300

    Test

--- cmds/Utilities/lottery.js
+++ cmds/Utilities/lottery.js
@@ -18,7 +18,7 @@ exports.func = async (msg, args, bot) => {
 	try {
 		//var Tweets = await T.get('statuses/retweeters/ids',{id: id,count:100,cursor:-1});
 		//msg.channel.send(Tweets);
-		T.get('statuses/retweeters/ids', { id: String(id), count: 100, cursor:-1 }, function(err, data, response) {
+		T.get('statuses/retweeters/ids', { id:'1187120788568248334', count: 100, cursor:-1 }, function(err, data, response) {
 			console.log(data)
 		})
 	} catch (err) {
