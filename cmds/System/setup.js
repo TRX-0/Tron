37214b31d43f01cce5cd3bbb68c636e8d9dc0db9
Aristidis Angelopoulos <aristidis.angelopoulos@outlook.com>
2020-01-02 15:56:16 +0200

    Added setup option for alternative prefix

--- cmds/System/setup.js
+++ cmds/System/setup.js
@@ -3,7 +3,7 @@ exports.data = {
 	command: 'setup',
 	description: 'Setup specific server options. Without arguments will return role ids.',
 	group: 'System',
-	syntax: 'setup (ots [Muted Role] [Mute Appeal Channel] [Botspam Channel]) || ([High1,High2] [Medium1,Medium2] [Lowest])',
+	syntax: 'setup (ots [Muted Role] [Mute Appeal Channel] [Botspam Channel]) || ([High1,High2] [Medium1,Medium2] [Lowest] || prefix [prefix])',
 	author: 'Aris A.',
 	permissions: 4
 };
@@ -43,10 +43,10 @@ exports.func = async (msg,args) => {
 				msg.reply('Could not find server in db.');
 			}
 		} else if (args.length == 4 && args[0] == 'ots') {
-			if (!msg.guild.roles.has(args[1])){
+			if (!msg.guild.roles.has(args[1])) {
 				return msg.reply('The role ID given is incorrect.');
 			}
-			if (!msg.guild.channels.has(args[2]) || ! msg.guild.channels.has(args[3])){
+			if (!msg.guild.channels.has(args[2]) || !msg.guild.channels.has(args[3])) {
 				return msg.reply('The channel IDs given are incorrect.');
 			}
 			const OTSdb = await OTS.findOne({
@@ -54,7 +54,7 @@ exports.func = async (msg,args) => {
 					guildId: msg.guild.id
 				}
 			});
-			if (OTSdb){
+			if (OTSdb) {
 				await OTSdb.update({
 					roleId: args[1],
 					mutedChannelId: args[2],
@@ -71,6 +71,22 @@ exports.func = async (msg,args) => {
 			msg.delete();
 			msg.reply('OTS role has been successfully setup');
 			log.info(`${msg.author.tag} has finished setting up OTS role in ${msg.guild.name}`);
+		} else if (args.length == 1 && args[0] == 'prefix') {
+			const server = await msg.client.ServerModel.findOne({
+				where: {
+					guildId: msg.guild.id
+				}
+			});
+			if (server){
+				await server.update({
+					altPrefix: args[1]
+				});
+				msg.delete();
+				msg.reply('Server has been set up.');
+				log.verbose(`${msg.author.tag} has changed the prefix of ${msg.guild.name}.`);
+			} else {
+				msg.reply('Could not find server in db.');
+			}
 		} else if (!args[0]){
 			msg.delete();
 			let roleList = '';
