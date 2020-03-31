4c359555b416924b2f5e91d5d6e6b3e135ab7f80
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2020-03-31 19:56:28 +0300

    Fixed a wrong variable name

--- cmds/Music/volume.js
+++ cmds/Music/volume.js
@@ -12,14 +12,14 @@ exports.func = async (message, args) => {
 	const log = require(`${message.client.config.folders.lib}/log.js`)(exports.data.name);
 	try {
 		if (args.length < 1) {
-			return msg.reply('You did not provide any arguments.');
+			return message.reply('You did not provide any arguments.');
 		}
 		const volume = parseInt(args[0]);
 		if (volume == NaN) {
-			return msg.reply('The volume must be a valid number.');
+			return message.reply('The volume must be a valid number.');
 		}
 		if (volume < '1' || volume > '200') {
-			return msg.reply('Volume must be between 10 and 200.');
+			return message.reply('Volume must be between 10 and 200.');
 		}
 		if (!message.member.voice.channel) {
 			return message.channel.send('You have to be in a voice channel to stop the music!');
