e986ef60d37d37626df53ad0c463f95dcef57795
Αριστείδης Αγγελόπουλος <aristidis.angelopoulos@outlook.com>
2020-03-31 19:27:53 +0300

    Hopefully the last fix for Volume

--- cmds/Music/volume.js
+++ cmds/Music/volume.js
@@ -10,21 +10,26 @@ exports.data = {
 
 exports.func = async (message, args) => {
 	const log = require(`${message.client.config.folders.lib}/log.js`)(exports.data.name);
-	if (args[0] == undefined) {
-		return msg.reply('You did not provide any arguments.');
+	try {
+		if (args.size < 1) {
+			return msg.reply('You did not provide any arguments.');
+		}
+		if (args[0] < '10' && args[0] > '200') {
+			return msg.reply('Volume must be between 10 and 200.');
+		}
+		if (!message.member.voice.channel) {
+			return message.channel.send('You have to be in a voice channel to stop the music!');
+		}
+		
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) {
+			return message.channel.send('There is no song currently playing!');
+		}
+		serverQueue.volume = args[0];
+		const volume = args[0] / 100;
+		serverQueue.connection.dispatcher.setVolume(volume);
+		message.channel.send(`Current volume is: ${args[0]}%`);
+	} catch (err) {
+		message.reply(`Something went wrong: ${err}`);
 	}
-	if (args[0] < 10 && args[0] > 100) {
-		return msg.reply('Volume must be between 10 and 100.');
-	}
-	const serverQueue = message.client.queue.get(message.guild.id);
-	if (!message.member.voice.channel) {
-		return message.channel.send('You have to be in a voice channel to stop the music!');
-	}
-	if (!serverQueue) {
-		return message.channel.send('There is no song currently playing!');
-	}
-	const volume = args[0] / 100;
-	serverQueue.volume = volume;
-	serverQueue.connection.dispatcher.setVolume(volume);
-	message.channel.send(`Current volume is: ${args[0]}%`);
 };
