42a08362d3b27678cafaa5b7660e0b6a216d028a
makelarisjr <8687447+makelarisjr@users.noreply.github.com>
2022-04-16 17:25:15 +0300

    cleanup category command

--- cmds/Utilities/category.js
+++ cmds/Utilities/category.js
@@ -10,6 +10,45 @@ exports.data = {
 
 exports.func = async (message,args) => {
 	const log = require(`${message.client.config.folders.lib}/log.js`)(exports.data.name);
+
+	const categories = [
+		{
+			id: '647428242516738049',
+			name: 'Web',
+			alias: []
+		},
+		{
+			id: '647428396787171338',
+			name: 'Pwn',
+			alias: ['binexp']
+		},
+		{
+			id: '647428478735745060',
+			name: 'Reversing',
+			alias: ['reverse']
+		},
+		{
+			id: '647428302071398442',
+			name: 'Crypto',
+			alias: []
+		},
+		{
+			id: '780147661465452654',
+			name: 'Hardware',
+			alias: []
+		},
+		{
+			id: '647428334719991831',
+			name: 'Forensics',
+			alias: []
+		},
+		{
+			id: '780147471249571900',
+			name: 'Blockchain',
+			alias: []
+		}
+	];
+
 	try{
 		if (message.guild.id != '630820897980416023') {
 			return message.reply('This command can only be used in the "INSSec Uniwa" Server.');
@@ -17,60 +56,21 @@ exports.func = async (message,args) => {
 		if (args[0] == undefined) {
 			return message.reply('Please provide a role to add.')
 		}
-		switch (args[0].toLowerCase()){
-			case 'web':
-				if (message.member.roles.cache.has('647428242516738049')) {
-					message.member.roles.remove('647428242516738049');
-				} else {
-					message.member.roles.add('647428242516738049');
-				}
-				break;
-			case 'pwn':
-				if (message.member.roles.cache.has('647428396787171338')) {
-					message.member.roles.remove('647428396787171338');
-				} else {
-					message.member.roles.add('647428396787171338');
-				}
-				break;
-			case 'reversing':
-				if (message.member.roles.cache.has('647428478735745060')) {
-					message.member.roles.remove('647428478735745060');
-				} else {
-					message.member.roles.add('647428478735745060');
-				}
-				break;
-			case 'crypto':
-				if (message.member.roles.cache.has('647428302071398442')) {
-					message.member.roles.remove('647428302071398442');
-				} else {
-					message.member.roles.add('647428302071398442');
-				}
-				break;
-			case 'hardware':
-				if (message.member.roles.cache.has('780147661465452654')) {
-					message.member.roles.remove('780147661465452654');
-				} else {
-					message.member.roles.add('780147661465452654');
-				}
-				break;
-			case 'forensics':
-				if (message.member.roles.cache.has('647428334719991831')) {
-					message.member.roles.remove('647428334719991831');
-				} else {
-					message.member.roles.add('647428334719991831');
-				}
-				break;
-			case 'blockchain':
-				if (message.member.roles.cache.has('780147471249571900')) {
-					message.member.roles.remove('780147471249571900');
-				} else {
-					message.member.roles.add('780147471249571900');
-				}
-				break;
-			default:
-				return message.reply('The role you specified is incorrect. Select from the following list: Web, Pwn, Crypto, Reverse, Forensic, Stego.')
+
+		let category = categories.find(c => c.name.toLowerCase() == args[0].toLowerCase() || c.alias.includes(args[0].toLowerCase()));
+
+		if (category === undefined) {
+			return message.reply(`The role you specified is incorrect. Select from the following list: ${categories.map(c => c.name).join(', ')}`);
 		}
-		message.reply('Role changes have been applied!');
+
+		if (message.member.roles.cache.has(category.id)) {
+			message.member.roles.remove(category.id);
+			return message.reply(`Removed the ${category.name} role.`);
+		}
+
+		await message.member.roles.add(category.id);
+
+		return message.reply(`You have been given the ${category.name} role.`);
 	} catch (err) {
 		message.reply('Something went wrong.');
 		log.error(`Something went wrong when choosing categories: ${err}`);
